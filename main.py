b2f7dc7c2135343c0dee7e5ed3bc9a1e42c81b88
hsoang <hoangle63808@yahoo.com>
2023-12-20 17:47:26 -0800

    Created methods

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-def leagueURLS():
-    Big5 = "https://fbref.com/en/comps/Big5/Big-5-European-Leagues-Stats#all_big5_table"
+def getTeamUrls():
+    Ligue1 = "https://fbref.com/en/comps/13/Ligue-1-Stats"
+    SerieA = "https://fbref.com/en/comps/11/Serie-A-Stats"
+    LaLiga = "https://fbref.com/en/comps/12/La-Liga-Stats"
+    Bundesliga = "https://fbref.com/en/comps/20/Bundesliga-Stats"
     Championship = "https://fbref.com/en/comps/10/Championship-Stats"
     Eredivisie = "https://fbref.com/en/comps/23/Eredivisie-Stats"
 
-    leaguesList = [Championship,Eredivisie]
+    leaguesList = [Championship]
     leaguesCount = len(leaguesList) #number of leagues to be used
     combinedLeaguesURLS = [] #urls of teams
 
@@ -40,13 +43,14 @@ def leagueURLS():
     return combinedLeaguesURLS
     
 
+
 # #print(combinedLeaguesURLS)
 # team_url = combinedLeaguesURLS[3]
 # #print(team_url)
 # data = requests.get(team_url)
 # soup = BeautifulSoup(data.text, features = "html.parser")
 
-# stats_table = soup.select('table.stats_table')[0]
+# stats_table = soup.select('table', class_='stats_table')[0]
 
 # links = stats_table.find_all('a')
 # links = [l.get("href") for l in links]
@@ -176,9 +180,103 @@ def leagueURLS():
 # print(stats[20].text)
 # print(stats[21].text)
 
+def normalize_name(name): #remove accents from names
+    name = name.replace("-"," ")
+    return unidecode(name)
+
+def playerStats(teamLinks):
+
+    for x in range(len(teamLinks)):
+        data = requests.get(teamLinks[x])
+        soup = BeautifulSoup(data.text, features = "html.parser")
+
+        stats_table = soup.select('table.stats_table')[0]
+
+        links = stats_table.find_all('a')
+        links = [l.get("href") for l in links]
+
+        links = [l for l in links if "/players/" in l and "/matchlogs/" not in l]
+        #print(links)
+
+        playerURLS = [f"https://fbref.com{l}" for l in links] #urls of players
+        #print(playerURLS)
+
+        for x in range(len(playerURLS)):
+            parts = playerURLS[x].split("/")
+            name = parts[-1]
+            nameURL = name.replace("-", " ")
+            #print(nameURL)
+
+            data = requests.get(playerURLS[x])
+            soup = BeautifulSoup(data.text, features = "html.parser")
+            Name = soup.find_all("span")
+            end = True
+            counter = 0
+
+            while (end):
+                if nameURL in normalize_name(Name[counter].text):
+                    Name = Name[counter]
+                    end = False
+                else:
+                    counter += 1
+
+            print(Name, end = " ")
+            shortInfo = soup.find_all("p")
+            end = 0
+            while end < 5:
+                if "Position" in shortInfo[end].text:
+                    shortInfo = shortInfo[end].text
+                    end = 5
+                else:
+                    end += 1
+
+            print(Position, end = " ")
+                    
+            parts = shortInfo.split("▪ ")
+            Position = parts[0].split(":")[1].strip()
+            Footed = parts[1].split(":")[1].strip()
+
+            #print(f"His position is: {Position}")
+            #print(f"His dominant foot is: {Footed}")
+                
+            Birthdate = soup.find_all("span", id = "necro-birth")
+            Birthdate = Birthdate[0].text
+            print(Birthdate, end = " ")
+
+            nationality = soup.find_all("p")
+            end = 0
+            while end < 5:
+                if "National Team" in nationality[end].text:
+                    nationality = nationality[end].text
+                    end = 5
+                else:
+                    end += 1
+
+            nationality = nationality.split(":")
+            NationalTeam = nationality[1].split(" ")
+            NationalTeam = NationalTeam[0].strip()
+            print(NationalTeam, end = " ")
+
+
+            clubInfo = soup.find_all("p")
+            end = 0
+            while end < 7:
+                if "Club:" in clubInfo[end].text:
+                    clubInfo= clubInfo[end].text
+                    end = 7
+                else:
+                    end += 1
+
+
+            clubInfo = clubInfo.split(":")
+            Club = clubInfo[1].strip()
+            print(Club, end = " ")
+
 
 if __name__ == '__main__':
     print("main")
 
-    leagues = leagueURLS()
-    print(leagues)
+    teamUrls = getTeamUrls()
+    print(teamUrls)
+
+    #playerStats(teamUrls)
